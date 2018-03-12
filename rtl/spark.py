42deb6d644c7cef68bf7513b582432fc15be9ef8
Matthijs van der Kroon <mvdkroon@cobr.io>
2018-03-12 11:30:28 +0100

    Improves SparkProcessor
    * added docstrings to the relevant methods
    * introduces a `format` method to parse url to brokertype, allowing:
        - different brokers for source and target
        - removes hardcoded kafka reference
    * adds a tempfile creator to ensure the program works across platforms

--- rtl/spark.py
+++ rtl/spark.py
@@ -5,35 +5,52 @@ from pyspark.streaming.kinesis import KinesisUtils
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import tempfile
 
-from base import Processor, Transformer, ParsedURL
+from rtl.client.base import ParsedURL
+from rtl.transformer.transformer import Transformer
+from rtl.processor import Processor
 
 
 class SparkProcessor(Processor):
     def __init__(self, source_url, transformer, target_url):
+        """Applies the transformer to a realtime feed using 
+        Apache Spark as execution engine. Supports either
+        Kafka or Kinesis.
+        
+        [description]
+        :param source_url: URL describing source data feed
+        :type source_url: string
+        :param transformer: Transformer
+        :type transformer: :class Transformer
+        :param target_url: URL describing destination data feed
+        :type target_url: string
+        """
         self.transformer = transformer
         self.source_url = ParsedURL(source_url)
         self.target_url = ParsedURL(target_url)
-        self._windowsize = 30
+        self._windowsize = 30 # Apache Spark microbatch streaming windowsize
 
     @staticmethod
-    def get_stream(brokertype, ssc):
-        if brokertype.lower() == "kafka":
-            return KafkaUtils.createStream(ssc)
-        elif brokertype.lower() == "kinesis":
-            return KinesisUtils.createStream(ssc)
-        else:
-            raise Exception("unsupported brokertype")
+    def format(url):
+        """Returns brokertype as a string, in the format that Apache 
+        Spark expects it.
+        
+        :param url: URL from which to extract the brokertype
+        :type url: string
+        """
+        return url.scheme.lower()
 
     def run(self):
+        """Start the stream transformation
+        """
         spark = (SparkSession.builder
             .appName(__name__)
             .getOrCreate())  
         
-        # TODO: Kinesis
         df = spark \
           .readStream \
-          .format("kafka") \
+          .format(SparkProcessor.format(self.source_url)) \
           .option("kafka.bootstrap.servers", self.source_url.netloc) \
           .option("subscribe", self.source_url.path[1:]) \
           .load()
@@ -41,15 +58,14 @@ class SparkProcessor(Processor):
         fn = udf(lambda s: self.transformer.map(s), StringType())
 
         df = df.withColumn('value', fn(df.value))
-
-        # TODO: Kinesis
-        ds = df \
-          .writeStream \
-          .format("kafka") \
-          .option("kafka.bootstrap.servers", self.target_url.netloc) \
-          .option("checkpointLocation", "/private/tmp") \
-          .option("topic", self.target_url.path[1:]) \
-          .start()
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            ds = df \
+              .writeStream \
+              .format(SparkProcessor.format(self.target_url)) \
+              .option("kafka.bootstrap.servers", self.target_url.netloc) \
+              .option("checkpointLocation", tmpdirname) \
+              .option("topic", self.target_url.path[1:]) \
+              .start()
 
         ds.awaitTermination()
 
